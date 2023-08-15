118b4335651b015a0d4f58a2b129d581399fe836
Pavel Kirilin <win10@list.ru>
2023-08-15 04:41:39 +0400

    Updated polybar config. Now it respects primary display.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- dotfiles/i3wm/polybar_launch.sh
+++ dotfiles/i3wm/polybar_launch.sh
@@ -6,7 +6,23 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Iterate over monitors and enable polybar for each.
-for m in $(xrandr --listmonitors | tail -n +2 | rev | cut -d ' ' -f1 | rev); do
-  MONITOR=$m polybar & disown
-done
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar.log
+
+PRIMARY_MON="$(xrandr --query | grep " connected" | grep "primary" | cut -d ' ' -f 1)"
+
+echo "Launching bar for primary monitor: $PRIMARY_MON"
+
+MONITOR=$PRIMARY_MON TRAY_POS=right polybar & disown
+
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | grep -v "primary" | cut -d ' ' -f 1); do
+    echo "Starting bar for monitor $m"
+    MONITOR=$m polybar & disown
+  done
+else
+  polybar --reload &
+fi
+
+echo "Bars launched..."
