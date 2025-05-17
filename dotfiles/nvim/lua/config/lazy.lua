345c1250a74d30f909a6f1842ba6c0a7f9a842b5
Pavel Kirilin <win10@list.ru>
2025-05-18 00:14:59 +0200

    Sync dotfiles

--- dotfiles/nvim/lua/config/lazy.lua
+++ dotfiles/nvim/lua/config/lazy.lua
@@ -34,8 +34,7 @@ require("lazy").setup({
       init = function()
         vim.cmd [[colorscheme catppuccin]]
       end
-    }
-
+    },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
