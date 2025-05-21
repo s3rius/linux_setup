bac8ce931ffd5845b55db027e339f758abfe2671
Pavel Kirilin <win10@list.ru>
2025-05-21 12:39:22 +0200

    Sync dotfiles

--- dotfiles/nvim/lua/plugins/utils.lua
+++ dotfiles/nvim/lua/plugins/utils.lua
@@ -169,4 +169,11 @@ return {
       end, { silent = true, noremap = true, desc = "Restore closed buffer" })
     end
   },
+{
+    "3rd/image.nvim",
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {
+        processor = "magick_cli",
+    }
+}
 }
